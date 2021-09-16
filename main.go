ebea11973b4f1f34927f0690c998b575a25a143a
Kendall M <kmeenan@intelligentdx.com>
2021-09-16 12:51:02 -0400

    Hello world

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Hello world function
+func HelloWorld(name string) {
+	fmt.Println("Hello, " + name + "!")
+}
+
+// Main function
+func main() {
+	HelloWorld("Kendall")
+}
